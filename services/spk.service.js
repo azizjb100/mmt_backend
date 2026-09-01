fa84a8e548d99322ac3fb9cab6299117180589a9
Aziz <azizjb100@gmail.com>
2026-09-01 09:32:45 +0700

    31/08/2026

--- services/spk.service.js
+++ services/spk.service.js
@@ -708,10 +708,8 @@ exports.getSpkForSublimLookup = async (keyword) => {
 
     // 1. FILTER LANGSUNG DI TINGKAT TABEL UTAMA (tspk & tmemospk)
     // Supaya MySQL TIDAK melakukan full scan ke seluruh histori SPK lama
-    let whereReguler =
-      "t.spk_aktif = 'Y' AND LOWER(t.spk_cab) IN ('p01', 'p04')";
-    let whereMemo =
-      "m.mspk_aktif = 'Y' AND LOWER(m.mspk_cab) IN ('p01', 'p04')";
+    let whereReguler = "t.spk_aktif = 'Y' AND t.spk_sublim = 'Y'";
+    let whereMemo = "m.mspk_aktif = 'Y' AND m.mspk_sublim = 'Y'";
 
     // 2. SUNTIKKAN KEYWORD LANGSUNG KE SUBQUERY
     if (keyword) {
