b879cd4598b8c9f0291dbf0e88ef850a74252bcf
Aziz <azizjb100@gmail.com>
2026-09-14 15:28:21 +0700

    14/09/2026

--- routes/manageUser.routes.js
+++ routes/manageUser.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require("../controllers/ManageUser.controller");
+const userCtrl = require("../controllers/manageUser.controller");
 
 // === 1. TARUH ENDPOINT STATIS / SPESIFIK DI ATAS ===
 router.get("/", userCtrl.browse);
